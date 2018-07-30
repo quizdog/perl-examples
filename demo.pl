b0d0b21f4bd4372053a78cc2a8f96a41d975d1fe
Mark Copley <mhc@quizdog.com>
2018-07-30 00:59:20 -0600

    made bob a Person

--- demo.pl
+++ demo.pl
@@ -1,11 +1,11 @@
 use Employee;
+use Person;
 
 $sara = new Employee "Sara", 27, "CEO";
 print $sara->display, "\n";
-$bob = new Employee "Bob", 27, "Engineer";
+
+$bob = new Person "Bob", 25;
 print $bob->display, "\n";
 
 $kid = $bob + $sara;
 print $kid->display, "\n";
-
-
